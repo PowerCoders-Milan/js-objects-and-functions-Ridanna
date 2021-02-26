a131dd0930de409c9881d14ad8f6f205c6618e68
mahmudref@gmail.com <mahmudref@gmail.com>
2021-02-26 14:34:09 +0100

    exe2

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,22 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function myFunction() {
+       
+var noun = prompt('write a noun')
+var verb = prompt('write a verb')
+var adverb = prompt('write an adverb')
+var adjective = prompt('write an adjective')
+alert(noun + verb + adverb + adjective)
+}
+myFunction()
+
+//function myFunction(noun, verb, adverb, adjective) {
+    //return noun+ verb+ adverb+ adjective;   
+//}
+//var noun = prompt('write a noun')
+//var verb = prompt('write a verb')
+//var adverb = prompt('write an adverb')
+//var adjective = prompt('write an adjective')
+
+//window.alert(myFunction (noun, verb, adverb, adjective ));//
--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,12 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function myFortune() {
+    var children = prompt('How many Kids do you like to have?')
+    var partername = prompt('with whoom would you like to marry?')
+    var geolocation = prompt('where would you like to live?')
+    var jobtitle =  prompt('in what positin woild you like to work?') 
+    alert('you will have ' + children + ' children with ' + partername + '.' + ' you will live in ' + geolocation + ' and work as ' + jobtitle + '.')
+}
+
+myFortune()
