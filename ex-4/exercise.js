21aae648328dbef69d887b7e895f36dea5a434e0
mahmudref@gmail.com <mahmudref@gmail.com>
2021-02-26 15:52:49 +0100

    exe4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,3 +1,9 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+function ageCalculator(birthYear) {
+    var currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+}
+document.write('your age is: ' + ageCalculator(1993) + ' years old.');
