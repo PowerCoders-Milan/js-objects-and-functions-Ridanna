0676da0ffcc668e0e3cbdeed90a8849688627b58
mahmudref@gmail.com <mahmudref@gmail.com>
2021-02-26 16:47:35 +0100

    recap

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -7,14 +7,14 @@ console.log(color[1] + ' is my second choice color!')
 console.log(color[2] + ' is my third choice color!')
 //another steps
 var favorite = {
-    color: 'blue',
-    pet: 'cat',
-    season: 'winter',
-    president: 'none'
+    color: 'blue ',
+    pet: ' cat ',
+    season: ' winter ',
+    president: ' Trump'
 
 }
 document.write(favorite.color)
 document.write(favorite.pet)
 document.write(favorite.season)
-
+document.write(favorite.president)
 
